7ff0ee418ef769db41c9b9a4d2ba64f9b1ca7701
ShreyanSen <shreyan.sen@gmail.com>
2024-02-29 23:05:28 +0530

    config filepath mod

--- src/tarot_deck.py
+++ src/tarot_deck.py
@@ -19,5 +19,9 @@ class TarotDeck:
         self.prompt = self.prompts[self.card]['Prompt']
 
     def open_config(self):
-        with open('config.yml', 'r') as file:
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        # Get the parent directory path (one level up)
+        config_dir = os.path.dirname(current_dir)
+        config_file_path = os.path.join(config_dir, 'config.yml')
+        with open(config_file_path, 'r') as file:
             self.prompts = yaml.safe_load(file)
